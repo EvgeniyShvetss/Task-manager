92c150b27299b6a579b5e1fa4f8e6d5cda4b0d74
Швец Евгений <shvets_evgeniy@MacBook-Pro-Svec.local>
2020-08-29 09:33:18 +0300

    Updates

---
+++ precache-manifest.291c0b006c37cc2b20905914948906f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9741a6f514f32b51a0d392990128b4d",
+    "url": "/Task-manager/index.html"
+  },
+  {
+    "revision": "68bf69cf0df4213ea981",
+    "url": "/Task-manager/static/css/main.d5ca450d.chunk.css"
+  },
+  {
+    "revision": "db3b9f0c9597056444c6",
+    "url": "/Task-manager/static/js/2.51d32c4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Task-manager/static/js/2.51d32c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68bf69cf0df4213ea981",
+    "url": "/Task-manager/static/js/main.5251b37c.chunk.js"
+  },
+  {
+    "revision": "1d58fd62b81624d0c3f9",
+    "url": "/Task-manager/static/js/runtime-main.db785bfc.js"
+  },
+  {
+    "revision": "3fdf77004c88a54f9c485a5a53dc9d68",
+    "url": "/Task-manager/static/media/resize.3fdf7700.jpg"
+  }
+]);
